030b1930c289961dcc52095642037c846fef6f8b
Kapil Kunt <75383738+kapil9236@users.noreply.github.com>
2021-02-10 08:04:53 +0530

    Declaring 2d Array

---
+++ Matrix/Vector of Vectors.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int main(){
+	
+	int m=3;
+	int n=2;
+	vector<vector<int> >arr;
+	
+	for(int i=0;i<m;i++){
+	vector<int>v;
+     for(int j=0; j<n;j++){
+     	 v.push_back(i+j);	
+	 }
+	 arr.push_back(v);
+	
+	}
+	
+	for(int i=0; i<m;i++){
+		for(int j=0; j<n; j++){
+			cout<<arr[i][j]<<"  ";
+		}
+	}
+	
+	
+	
+	
+	return 0;
+}
+
