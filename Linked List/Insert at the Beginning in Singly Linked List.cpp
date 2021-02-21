f772ff06676fee58077bb4a343d72eb76f7a10c7
Kapil Kunt <75383738+kapil9236@users.noreply.github.com>
2021-02-21 13:32:17 +0530

    Insert at Begin

---
+++ Linked List/Insert at the Beginning in Singly Linked List.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+
+struct Node{
+	int data;
+	Node *next;
+	Node(int x){
+		data=x;
+		next=NULL;
+	}
+};
+
+Node *insertBegin( Node *head, int x){
+	Node *temp= new Node(x);
+	temp->next=head;
+	return temp;
+}
+
+void printList( Node *head){
+	Node *curr= head;
+	while(curr!=NULL){
+		cout<<curr->data<<" ";
+		curr=curr->next;
+	}
+}
+
+
+int main(){
+	
+	Node *head=NULL;
+	head=insertBegin(head,30);
+	head=insertBegin(head,20);
+	head=insertBegin(head,10);
+	head=insertBegin(head,45);
+	printList(head);
+	cout<<endl;
+	cout<<"our current head data part is "<<head->data;
+	return 0;
+	
+	
+}
