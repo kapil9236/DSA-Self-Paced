30fb6354a21454e124515588841887fc0deeaf65
Kapil Kunt <75383738+kapil9236@users.noreply.github.com>
2021-02-21 15:08:37 +0530

    Delete Last Node

---
+++ Linked List/Delete Last Node in Singly Linked List.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+
+struct Node{
+	int data;
+	Node *next;
+	Node(int x){
+		data=x;
+		next=NULL;
+	}
+};
+
+Node *insertEnd( Node *head, int x){
+	Node *temp= new Node(x);
+	if(head==NULL)
+	return temp;
+	Node *curr=head;
+	while(curr->next!= NULL){
+		curr=curr->next;	
+	}
+	curr->next=temp;
+	return head;
+}
+
+ Node *delLast(Node *head){
+ 	if(head==NULL){
+ 		return NULL;
+		 }
+	if(head->next==NULL){
+		delete(head);
+		return NULL;
+	}	 
+ 	
+ 	Node *curr=head;
+ 	while(curr->next->next!=NULL){
+ 		curr=curr->next;
+	 }
+	delete(curr->next);
+	curr->next=NULL;
+	return head; 
+	 
+ }
+
+
+void printList( Node *head){
+	Node *curr= head;
+	while(curr!=NULL){
+		cout<<curr->data<<" ";
+		curr=curr->next;
+	}
+}
+
+
+int main(){
+	
+	Node *head=NULL;
+	head=insertEnd(head,30);
+	head=insertEnd(head,20);
+	head=insertEnd(head,10);
+	head=insertEnd(head,45);
+	
+    head=delLast(head);
+	printList(head);
+	cout<<endl;
+	cout<<"our current head data part is "<<head->data;
+	return 0;
+	
+	
+}
